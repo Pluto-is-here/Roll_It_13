110d895a4124543bdc68f224cc338c5bc591a4e7
Pluto-is-here <141596344+Pluto-is-here@users.noreply.github.com>
2024-02-23 10:14:02 +1300

    Stats component working

---
+++ C_08_Stats.py
@@ -0,0 +1,44 @@
+# find the lowest. highest and average score from a list
+def get_stats(stats_list):
+    # sort the lists
+    stats_list.sort()
+
+    # calculate the lowest, highest and average score and display them
+    lowest_score = stats_list[0]
+    highest_score = stats_list[-1]
+    average_score = sum(stats_list) / len(stats_list)
+
+    return [lowest_score, highest_score, average_score]
+
+
+# create lists to hold user and computer scores
+
+user_scores = [10, 0, 13, 7, 10, 11]
+comp_scores = [10, 11, 0, 0, 10, 11]
+# Loop six times - for testing purpose, ask the user to enter the score for the
+# for item in range(0, 6):
+#     user_score = int(input("Enter the user score: "))
+#     comp_score = int(input("Enter the computer score: "))
+#
+#     # add user & computer score to lists!
+#
+#     user_scores.append(user_score)
+#     comp_scores.append(comp_score)
+
+
+# sort the lists
+user_scores.sort()
+comp_scores.sort()
+
+# calculate the lowest, highest and average score and display them
+user_stats = get_stats(user_scores)
+comp_stats = get_stats(comp_scores)
+
+print(" *** Game statistics *** ")
+print(f"User     - Lowest Score: {user_stats[0]}\t "
+      f"Highest Score: {user_stats[1]}\t "
+      f"Average Score: {user_stats[2]}")
+
+print(f"Computer - Lowest Score: {comp_stats[0]}\t "
+      f"Highest Score: {comp_stats[1]}\t "
+      f"Average Score: {comp_stats[2]}")
